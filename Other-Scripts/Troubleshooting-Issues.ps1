ca9e4a16916e91ed131b37ba45e6b7e6ee39216e
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2023-01-07 12:47:35 +0530

    updated details

---
+++ Other-Scripts/Troubleshooting-Issues.ps1
@@ -0,0 +1,10 @@
+# Issue# 01 - JBL Bluetooth not connecting
+https://askubuntu.com/a/1408452/1015062
+
+sudo apt-get install blueman
+sudo /etc/init.d/bluetooth start
+
+sudo apt-get update -y
+sudo apt-get install -y bluez-tools
+
+# Issue# 02 - 
