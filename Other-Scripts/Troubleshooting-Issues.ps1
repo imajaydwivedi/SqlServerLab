6a2974c3909da68f533d193498f9c0970a454c91
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2023-01-22 20:58:28 +0530

    Updated link

--- Other-Scripts/Troubleshooting-Issues.ps1
+++ Other-Scripts/Troubleshooting-Issues.ps1
@@ -13,4 +13,9 @@ https://ubuntu-mate.community/t/sharing-folders-on-windows-network/2644
 sudo apt install -y caja-share samba
 reboot now
 
+# Samba Share setup properly, and Windows not able to access
+Error/Fix => https://learn.microsoft.com/en-us/troubleshoot/windows-client/networking/cannot-access-shared-folder-file-explorer#you-cant-access-this-shared-folder-because-your-organizations-security-policies-block-unauthenticated-guest-access
+https://learn.microsoft.com/en-us/troubleshoot/windows-client/networking/cannot-access-shared-folder-file-explorer#:~:text=Method%202%3A%20Enable%20insecure%20guest%20logons%20with%20Local%20Group%20Policy%20Editor
+
+
 
