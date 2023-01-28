1567145b467a5d7c7ec5522feaf216dc42c50376
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2023-01-28 21:46:28 +0530

    Added grub file

---
+++ Other-Scripts/enable-hibernation.sh
@@ -0,0 +1,10 @@
+# https://www.linuxuprising.com/2021/08/how-to-enable-hibernation-on-ubuntu.html
+
+# Find out UUID of disk contining Swap File
+swapon --show # Note swap file location & use in below command
+findmnt -no UUID -T /hyper-active/swapfile.img
+# 4ab3a57f-7a48-4d3b-8db6-4015be68981e
+
+# Find out the swap file offset
+sudo filefrag -v /hyper-active/swapfile.img
+# 163479552
