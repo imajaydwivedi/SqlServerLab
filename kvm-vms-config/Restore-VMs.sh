394f539fb48cfc608145d436fbbe6c2532a17da0
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-04-06 21:45:00 +0530

    restore cmds

--- kvm-vms-config/Restore-VMs.sh
+++ kvm-vms-config/Restore-VMs.sh
@@ -4,3 +4,13 @@ virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/Experiment
 virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/PgProd.xml
 virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/SqlPractice.xml
 virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/Workstation.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/SQLExpress.xml
+
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/AgHost-1A.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/AgHost-1B.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/AgHost-2A.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/AgHost-2B.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/SqlProd-A.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/SqlProd-B.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/SqlDr-A.xml
+virsh define --file /stale-storage/GitHub/SqlServerLab/kvm-vms-config/SqlDr-B.xml
