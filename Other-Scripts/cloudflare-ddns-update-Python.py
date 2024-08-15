2db7bc1f66258978c77d878b4cea9ddefee3016a
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2024-08-15 18:04:09 +0530

    updated ip update command

--- Other-Scripts/cloudflare-ddns-update-Python.py
+++ Other-Scripts/cloudflare-ddns-update-Python.py
@@ -12,13 +12,15 @@ new_ip_address = socket.gethostbyname(noip_domain)
 print(f'new_ip_address => {new_ip_address}')
 
 cf = CloudFlare(email, api_key, sqlmonitor_subdomain)
-old_ip_address = cf.get_record('A', sqlmonitor_subdomain)
+dns_a_record_info = cf.get_record('A', sqlmonitor_subdomain)
+old_ip_address = dns_a_record_info['content']
+print(f'old_ip_address => {old_ip_address}\n')
 
 if old_ip_address == new_ip_address:
-  print("Update of ip address is required.")
-  cf.update_record('A',sqlmonitor_subdomain,new_ip_address)
-else:
   print("Active ip address is correct.")
+else:
+  print(f"Update of ip address is required from {old_ip_address} to {new_ip_address}.")
+  cf.update_record('A',sqlmonitor_subdomain,new_ip_address)
 
 '''
 python ~/GitHub/SqlServerLab/Private/cloudflare-ddns-update-Python.py 
@@ -29,7 +31,7 @@ Save following content in above cron file
 #https://stackoverflow.com/a/21648491/4449743
 
 # run script every 5 minutes
-*/5 * * * *   saanvi  python ~/GitHub/SqlServerLab/Private/cloudflare-ddns-update-Python.py
+*/5 * * * *   saanvi  
 
 # run script after system (re)boot
 @reboot       saanvi  python ~/GitHub/SqlServerLab/Private/cloudflare-ddns-update-Python.py
